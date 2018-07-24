fe1f2b347a0f685e88dd16ae0ba8ac7a7f44ec1b
madeline57 <maddiejobeth@aol.com>
2018-07-24 18:56:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,15 @@
 def fizzbuzz(number)
-if number % 5 ==0
-  puts "fizz"
+if number % 15 ==0
+  puts "fizzbuzz"
 elsif number % 3 ==0
   puts "buzz"
-elsif number % 15 ==0  
-  puts "fizzbuzz"
+elsif number % 5 ==0  
+  puts "fizz"
 else number 
   puts"non"
 end
 end 
+
+
 fizzbuzz(9)
 fizzbuzz(15)
